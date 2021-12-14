38bf9139d46cb9b314f629905169cac0a197b47e
Julian Yap <julianokyap@gmail.com>
2021-12-14 15:38:11 -0800

    Refactor

--- client.go
+++ client.go
@@ -9,17 +9,62 @@ import (
 	"time"
 )
 
-type client struct {
+const (
+	apiURL            = "https://api.exchange.coinbase.com/" // Coinbase Pro API endpoint
+	httpClientTimeout = 30                                   // HTTP client timeout
+)
+
+// Client holds information necessary to make a request to your API
+type Client struct {
+	baseURL    string
 	httpClient *http.Client
 }
 
-// NewClient return a new Coinbase HTTP client
-func NewClient() (c *client) {
-	return &client{&http.Client{}}
+// Option is a functional option for configuring the API client
+type Option func(*Client) error
+
+// BaseURL allows overriding of API client baseURL for testing
+func BaseURL(baseURL string) Option {
+	return func(c *Client) error {
+		c.baseURL = baseURL
+		return nil
+	}
+}
+
+// New creates a new API client
+func New(opts ...Option) *Client {
+	client := &Client{
+		baseURL: apiURL,
+		httpClient: &http.Client{
+			Timeout: time.Second * httpClientTimeout,
+		},
+	}
+
+	if err := client.parseOptions(opts...); err != nil {
+		return nil
+	}
+
+	return client
+}
+
+// parseOptions parses the supplied options functions and returns a configured
+// *Client instance
+func (c *Client) parseOptions(opts ...Option) error {
+	// Range over each options function and apply it to our API type to
+	// configure it. Options functions are applied in order, with any
+	// conflicting options overriding earlier calls.
+	for _, option := range opts {
+		err := option(c)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // doTimeoutRequest do a HTTP request with timeout
-func (c *client) doTimeoutRequest(timer *time.Timer, req *http.Request) (*http.Response, error) {
+func (c *Client) doTimeoutRequest(timer *time.Timer, req *http.Request) (*http.Response, error) {
 	// Do the request in the background so we can check the timeout
 	type result struct {
 		resp *http.Response
@@ -40,14 +85,14 @@ func (c *client) doTimeoutRequest(timer *time.Timer, req *http.Request) (*http.R
 }
 
 // do prepare and process HTTP request to Coinbase API
-func (c *client) do(method string, resource string, payload string, authNeeded bool) (response []byte, err error) {
-	connectTimer := time.NewTimer(DEFAULT_HTTPCLIENT_TIMEOUT * time.Second)
+func (c *Client) do(method string, resource string, payload string, authNeeded bool) (response []byte, err error) {
+	connectTimer := time.NewTimer(httpClientTimeout * time.Second)
 
 	var rawurl string
 	if strings.HasPrefix(resource, "http") {
 		rawurl = resource
 	} else {
-		rawurl = fmt.Sprintf("%s%s", API_BASE, resource)
+		rawurl = fmt.Sprintf("%s%s", apiURL, resource)
 	}
 
 	req, err := http.NewRequest(method, rawurl, strings.NewReader(payload))
--- coinbasepro.go
+++ coinbasepro.go
@@ -5,26 +5,10 @@ import (
 	"encoding/json"
 )
 
-const (
-	API_BASE                   = "https://api.exchange.coinbase.com/" // Coinbase Pro API endpoint
-	DEFAULT_HTTPCLIENT_TIMEOUT = 30                                   // HTTP client timeout
-)
-
-// New return a instanciate coinbase struct
-func New() *CoinbasePro {
-	client := NewClient()
-	return &CoinbasePro{client}
-}
-
-// coinbase represent a coinbase client
-type CoinbasePro struct {
-	client *client
-}
-
 // https://api.exchange.coinbase.com/currencies
 // Get all known currencies
-func (b *CoinbasePro) GetCurrencies() (currencies []Currency, err error) {
-	r, err := b.client.do("GET", "currencies", "", false)
+func (b *Client) GetCurrencies() (currencies []Currency, err error) {
+	r, err := b.do("GET", "currencies", "", false)
 	if err != nil {
 		return
 	}
@@ -39,8 +23,8 @@ func (b *CoinbasePro) GetCurrencies() (currencies []Currency, err error) {
 }
 
 // https://api.exchange.coinbase.com/products/stats
-func (b *CoinbasePro) GetStats() (stats map[string]Stat, err error) {
-	r, err := b.client.do("GET", "products/stats", "", false)
+func (b *Client) GetStats() (stats map[string]Stat, err error) {
+	r, err := b.do("GET", "products/stats", "", false)
 	if err != nil {
 		return
 	}
