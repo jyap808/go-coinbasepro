5d70c7dbb62c34cff53b78c2bca35fb917f6d943
Julian Yap <julianokyap@gmail.com>
2021-12-14 21:04:20 -0800

    Add GetStats test case

--- coinbasepro_test.go
+++ coinbasepro_test.go
@@ -63,8 +63,12 @@ func TestClient_GetCurrencies(t *testing.T) {
 				httpClient: client.httpClient,
 			},
 			wantCurrencies: []Currency{
-				{Id: "OXT", Name: "Orchid", Details: Details{json.Number("35")}},
-				{Id: "IOTX", Name: "IoTeX (ERC-20)", Details: Details{json.Number("35")}},
+				{Id: "OXT", Name: "Orchid",
+					Details: Details{
+						NetworkConfirmations: json.Number("35")}},
+				{Id: "IOTX", Name: "IoTeX (ERC-20)",
+					Details: Details{
+						NetworkConfirmations: json.Number("35")}},
 			},
 			wantErr: false,
 		},
@@ -82,6 +86,57 @@ func TestClient_GetCurrencies(t *testing.T) {
 			}
 			if !reflect.DeepEqual(gotCurrencies, tt.wantCurrencies) {
 				t.Errorf("Client.GetCurrencies() = %v, want %v", gotCurrencies, tt.wantCurrencies)
+			}
+		})
+	}
+}
+
+func TestClient_GetStats(t *testing.T) {
+	teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/products/stats", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, fixture("stats.json"))
+	})
+
+	type fields struct {
+		baseURL    string
+		httpClient *http.Client
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		wantStats map[string]Stat
+		wantErr   bool
+	}{
+		{
+			name: "Test",
+			fields: fields{
+				baseURL:    client.baseURL,
+				httpClient: client.httpClient,
+			},
+			wantStats: map[string]Stat{
+				"BCH-BTC": {
+					Stats24Hour{
+						Volume: json.Number("3263.97247188"), Last: json.Number("0.00902")}}},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := &Client{
+				baseURL:    tt.fields.baseURL,
+				httpClient: tt.fields.httpClient,
+			}
+			gotStats, err := b.GetStats()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Client.GetStats() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotStats, tt.wantStats) {
+				t.Errorf("Client.GetStats() = %v, want %v", gotStats, tt.wantStats)
 			}
 		})
 	}
